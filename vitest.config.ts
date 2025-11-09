b97159218fa50bbdf0f450f3778c084dc9366ba9
Starpuccino <854408292@qq.com>
2025-11-10 00:35:14 +0800

    feat: 添加vitest配置文件以支持测试环境

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import path from 'node:path'
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    reporters: 'default',
+    coverage: {
+      reporter: ['text', 'html'],
+      reportsDirectory: 'coverage'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.')
+    }
+  }
+})
