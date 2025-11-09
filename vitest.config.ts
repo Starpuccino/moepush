2ac0dde0d63224b284eb65009e4479b023c69c01
Starpuccino <854408292@qq.com>
2025-11-10 01:13:53 +0800

    feat: 更新vitest配置，确保测试环境不自动监视文件变化

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     environment: 'node',
     reporters: 'default',
+    watch: false,
     coverage: {
       reporter: ['text', 'html'],
       reportsDirectory: 'coverage'
